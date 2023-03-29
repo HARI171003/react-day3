78042963a068338f0822b991fd6545d1a7250920
HARI171003 <727721euec042@skcet.ac.in>
2023-03-29 14:42:23 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Components/home.css"
+export default function Home() {
+  return (
+    <div>
+        <div className='div-main'>
+           <h1>
+           Good Coffee</h1><br/>
+               <h2>For people And Planet</h2>
+            <p> Stressed, blessed, and coffee obsessed</p><br/><br/>
+            <a href=''>Read More</a>
+        </div>
+    </div>
+  )
+}
